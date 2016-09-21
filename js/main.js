b7ffa8c601357f76a415321d1fbec3bde58f2410
user-PC\user <youngj6169@gmail.com>
2016-09-21 16:05:58 +0900

    Add minibar, testing responsive index, profile.html

--- js/main.js
+++ js/main.js
@@ -46,9 +46,9 @@
   menu icon slide-out
 */
 $('#menu-icon').on('click', function(){
-  $('.content').toggleClass('side-bar-out');
-  $('.side-bar').toggleClass('side-bar-out');
-  $('#menu-icon').toggleClass('side-bar-out');
+  $('.content').toggleClass('side-bar-in');
+  $('.side-bar').toggleClass('side-bar-in');
+  $('#menu-icon').toggleClass('side-bar-in');
 })
 
 
@@ -59,15 +59,15 @@ var Index = {
   "description": [
     {
       "name": "Web Developer",
-      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis."
+      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
     },
     {
       "name": "UX Developer",
-      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis."
+      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. "
     },
     {
       "name": "Information Visualization",
-      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis."
+      "des": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis vero facere inventore voluptatibus ipsum minima. Ipsa repellendus pariatur quis, veniam distinctio itaque vitae eum doloremque suscipit harum tempore dignissimos officia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde laudantium aperiam molestias, at ipsam! Magnam eveniet, assumenda labore nemo dignissimos quaerat expedita ex facere laboriosam dolore vero quasi eius, debitis. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     }
   ],
   "display" : function() {
